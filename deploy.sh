f0d596e17b28f092e1a23c1bb2b94e9ecec17571
YonatanFurmanAssa <james8999123456789@gmail.com>
2022-11-19 23:40:45 +0200

    1222

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,13 @@ machine=$1
 jenkins_folder="/var/lib/jenkins/workspace/dev-Automation/docker-compose.yml"
 echo "deploying to $machine"
 echo "createing directory and copy"
-scp -o StrictHostKeyChecking=no -r $jenkins_folder ec2-user@$machine:/home/ec2-user/final
+scp -o StrictHostKeyChecking=no -r $jenkins_folder ec2-user@$machine:/home/ec2-user/bynet
 scp -o StrictHostKeyChecking=no -r ~/.docker/config.json ec2-user@$machine:~/.docker/config.json
 echo "COPIED to $machine"
 ssh ec2-user@$machine "docker login"
 ssh ec2-user@$machine "docker pull yonatanfurmandocker/bynets_server2:1.0"
 ssh ec2-user@$machine "docker pull yonatanfurmandocker/bynets_app2:1.0"
-ssh ec2-user@$machine "docker-compose -f /home/ec2-user/final/docker-compose.yaml up -d"
+ssh ec2-user@$machine "docker-compose -f /home/ec2-user/bynet/docker-compose.yaml up -d"
 if [ $machine == "test" ];
 then
     sleep 20
@@ -22,6 +22,6 @@ then
         echo 'The Test Failed Unfortunately'
     fi
     
-    ssh ec2-user@$machine "docker-compose -f /home/ec2-user/final/docker-compose.yaml down"
+    ssh ec2-user@$machine "docker-compose -f /home/ec2-user/bynet/docker-compose.yaml down"
     echo 'Test Docker Finished !' 
 fi
