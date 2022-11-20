42ae1608e9b75fc09813367a0913a0adf3db7ab0
YonatanFurmanAssa <james8999123456789@gmail.com>
2022-11-20 16:12:00 +0200

    1222

--- Frontend/src/Utils/Config.ts
+++ Frontend/src/Utils/Config.ts
@@ -9,14 +9,14 @@ class DevelopmentConfig extends Config {
     public meetingsTypeURL = "http://54.227.120.82:3002/api/meetings-type";
     public meetingsByOrganizerUrl = "http://54.227.120.82:3002/api/meetings-by-organizer/";
     public meetingsURL = "http://54.227.120.82:3002/api/meetings/";
-    public organizersURL = "http://54.227.120.82:3002/api/organizers/";
+    public organizersURL = "http://ec2-54-227-120-82.compute-1.amazonaws.com:3002/";
 }
 
 class ProductionConfig extends Config {
     public meetingsTypeURL = "http://54.227.120.82:3002/api/meetings-type";
     public meetingsByOrganizerUrl = "http://54.227.120.82:3002/api/meetings-by-organizer/";
     public meetingsURL = "http://54.227.120.82:3002/api/meetings/";
-    public organizersURL = "http://54.227.120.82:3002/api/organizers/";
+    public organizersURL = "http://ec2-54-227-120-82.compute-1.amazonaws.com:3002/";
 }
 
 const config = process.env.NODE_ENV === "development" ? new DevelopmentConfig() : new ProductionConfig();
