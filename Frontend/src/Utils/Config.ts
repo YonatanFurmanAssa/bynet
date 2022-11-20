8b2517e71764a2dd421e53e81a05bc91258db9b4
YonatanFurmanAssa <james8999123456789@gmail.com>
2022-11-20 15:51:15 +0200

    1222

--- Frontend/src/Utils/Config.ts
+++ Frontend/src/Utils/Config.ts
@@ -6,17 +6,17 @@ class Config {
 }
 
 class DevelopmentConfig extends Config {
-    public meetingsTypeURL = "http://localhost:3002/api/meetings-type";
-    public meetingsByOrganizerUrl = "http://localhost:3002/api/meetings-by-organizer/";
-    public meetingsURL = "http://localhost:3002/api/meetings/";
-    public organizersURL = "http://localhost:3002/api/organizers/";
+    public meetingsTypeURL = "http://54.227.120.82:3002/api/meetings-type";
+    public meetingsByOrganizerUrl = "http://54.227.120.82:3002/api/meetings-by-organizer/";
+    public meetingsURL = "http://54.227.120.82:3002/api/meetings/";
+    public organizersURL = "http://54.227.120.82:3002/api/organizers/";
 }
 
 class ProductionConfig extends Config {
-    public meetingsTypeURL = `http://${process.env.SERVER_URL}:${process.env.SERVER_PORT}/api/meetings-type/`;
-    public meetingsByOrganizerUrl = `http://${process.env.SERVER_URL}:${process.env.SERVER_PORT}/api/meetings-by-organizer/`;
-    public meetingsURL = `http://${process.env.SERVER_URL}:${process.env.SERVER_PORT}/api/meetings/`;
-    public organizersURL = `http://${process.env.SERVER_URL}:${process.env.SERVER_PORT}/api/organizers/`;
+    public meetingsTypeURL = "http://54.227.120.82:3002/api/meetings-type";
+    public meetingsByOrganizerUrl = "http://54.227.120.82:3002/api/meetings-by-organizer/";
+    public meetingsURL = "http://54.227.120.82:3002/api/meetings/";
+    public organizersURL = "http://54.227.120.82:3002/api/organizers/";
 }
 
 const config = process.env.NODE_ENV === "development" ? new DevelopmentConfig() : new ProductionConfig();
