f56ee7151b390ada45e6092f9fe352a09d03270f
YonatanFurmanAssa <james8999123456789@gmail.com>
2022-11-20 23:10:32 +0200

    new10

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,7 @@ else
     echo "createing directory and copy"
     ssh ec2-user@$machine "mkdir bynet"
     scp -o StrictHostKeyChecking=no -r $jenkins_folder ec2-user@$machine:/home/ec2-user/bynet/docker-compose.yaml
+    scp -o StrictHostKeyChecking=no -r ~/.docker/config.json ec2-user@$machine:~/.docker/config.json
     echo "COPIED to $machine"
     ssh ec2-user@$machine "echo $DOCKERHUB_CREDENTIALS_PSW | sudo docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin"
     ssh ec2-user@$machine "cd bynet"
